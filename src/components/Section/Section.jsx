3afdf31d623b0708a673157cb7b32bf2d306a3b1
Yashwanth <mangaliyeswanth@gmail.com>
2025-07-04 19:18:49 +0530

    Button

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -6,6 +6,11 @@ import web from '../../assets/github.png';
 import resume from '../../assets/Yashwanth.pdf'
 
 const Section = () => {
+
+    const button = () =>{
+        alert("Resume Downloaded\n Please check it😊")
+    }
+
     return (
         <section>
             <div className='intro'>
@@ -18,8 +23,8 @@ const Section = () => {
                         Front-end developer skilled in React.js, JavaScript, HTML, and CSS with a solid foundation in Data Structures and Algorithms. Passionate about building responsive, user-friendly interfaces and exploring modern frameworks like Next.js.
                     </span><br />
                     <a href={web} download>
-                    <button className='btn' aria-label="Download CV">
-                        <img src={download} alt="Download icon" className='download' /><a href={resume} download> Download CV</a>
+                    <button className='btn' aria-label="Download CV" onClick={button}>
+                        <img src={download} alt="Download icon" className='download' /><a href={resume} download> Download Resume</a>
                     </button>
                     </a>
                 </div>
